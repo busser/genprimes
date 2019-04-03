d66a8b64f199f7fdab97f98f40e67ab29505e37e
Arthur Busser <git@busser.me>
2019-04-03 14:19:46 +0200

    Add larger tests and benchmarks

--- main_test.go
+++ main_test.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 	"reflect"
+	"strconv"
 	"testing"
 )
 
@@ -24,6 +28,7 @@ func TestGenPrimes(t *testing.T) {
 		{3, []int{2, 3, 5}},
 		{6, []int{2, 3, 5, 7, 11, 13}},
 		{10, []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29}},
+		{9592, fixture("testdata/first-9592-primes.txt")},
 	}
 
 	for _, genPrimes := range funcs {
@@ -45,7 +50,7 @@ func TestGenPrimes(t *testing.T) {
 
 func BenchmarkGenPrimes(b *testing.B) {
 	for _, genPrimes := range funcs {
-		for count := 1; count <= 1024; count *= 2 {
+		for count := 1; count <= 4*1024; count *= 2 {
 			b.Run(fmt.Sprintf("%s/%d", genPrimes.name, count), func(b *testing.B) {
 				for i := 0; i < b.N; i++ {
 					genPrimes.f(count, func(p int) {})
@@ -53,4 +58,28 @@ func BenchmarkGenPrimes(b *testing.B) {
 			})
 		}
 	}
+}
+
+func fixture(filename string) []int {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("failed to open %s: %v", filename, err)
+	}
+
+	ints := []int{}
+
+	s := bufio.NewScanner(f)
+
+	for s.Scan() {
+		i, err := strconv.Atoi(s.Text())
+		if err != nil {
+			log.Fatalf("failed to parse %v as int: %v", s.Text(), err)
+		}
+		ints = append(ints, i)
+	}
+	if err := s.Err(); err != nil {
+		log.Fatalf("failed to read file contents: %v", err)
+	}
+
+	return ints
 }
